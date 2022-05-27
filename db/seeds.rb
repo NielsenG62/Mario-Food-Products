bb9f3ad086df2e8231f0581a5c8293cd65c60b26
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-27 15:45:23 -0700

    add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.ingredient,
+                    cost: Faker::Number.between(from: 1, to: 25),
+                    country_of_origin: Faker::Address.country_code_long)
+end
+
+250.times do |index|
+  Review.create!(author: Faker::Name.name,
+                    rating: Faker::Number.between(from: 1, to: 5),
+                    product_id: Faker::Number.between(from: 1, to: 50),
+                    content_body: Faker::Lorem.paragraph_by_chars(number: 100, supplemental: false))
+end
+
+p "Created #{Product.count} products and #{Review.count} reviews"
