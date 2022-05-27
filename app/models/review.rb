3efd3aa8402f8ffab8a2233f4656ab076fb80bf3
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-27 11:01:18 -0700

    add test and code for review rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
   validates :rating, presence: true
+  validates :rating, :inclusion => 1..5
   validates :content_body, presence: true
   validates :content_body, length: { in: 50..250 }
 end
